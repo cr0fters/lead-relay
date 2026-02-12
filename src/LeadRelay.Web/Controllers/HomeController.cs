07bf15b02c0ae3f2d9ed3a48a33be93481d1d040
andrew.moorcroft <andrew.moorcroft@enquirylabs.co.uk>
2026-02-12 22:56:50 +0000

    added live site id

--- src/LeadRelay.Web/Controllers/HomeController.cs
+++ src/LeadRelay.Web/Controllers/HomeController.cs
@@ -3,17 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LeadRelay.Web.Controllers;
 
-public sealed class HomeController(ISiteRepository sites) : Controller
+public sealed class HomeController : Controller
 {
     [HttpGet("/")]
-    public async Task<ViewResult> Index(CancellationToken ct)
+    public ViewResult Index(CancellationToken ct)
     {
-        var siteId = (await sites.GetAllAsync(ct)).FirstOrDefault()?.Id ?? "";
-        return View(new HomeViewModel { WidgetSiteId = siteId });
-    }
-
-    public sealed class HomeViewModel
-    {
-        public string WidgetSiteId { get; set; } = "";
+        return View();
     }
 }
